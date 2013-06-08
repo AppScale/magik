0c46319c12f7352bc2349355dd7ccd97a5eaadf2
Chris Bunch <shatterednirvana@gmail.com>
2013-06-08 12:22:25 -0700

    defining v1 basestorage interface

--- magik/base_storage.py
+++ magik/base_storage.py
@@ -23,3 +23,37 @@ class BaseStorage():
         connect to the storage system.
     """
     raise NotImplementedError
+
+
+  def upload_files(self, source_to_dest_list):
+    """ Uploads one or more files to the storage platform.
+
+    Args:
+      source_to_dest_list: A list of dicts, where each dict has a key named
+        'source' that points to the file on the local filesystem to upload,
+        and a key named 'destination' that points to where it should be
+        uploaded on the remote storage service.
+    Returns:
+      A copy of the same list of dicts that was passed in as an argument,
+        with an extra field in each dict named 'success', that indicates if
+        the upload was successful, and in case of failures, a field called
+        'failure_reason' that explains why the file could not be uploaded.
+    """
+    raise NotImplementedError
+
+
+  def download_files(self, source_to_dest_list):
+    """ Downloads one or more files from the storage platform.
+
+    Args:
+      source_to_dest_list: A list of dicts, where each dict has a key named
+        'source' that points to the file on the storage platform to download,
+        and a key named 'destination' that points to where it should be
+        downloaded on the local filesystem.
+    Returns:
+      A copy of the same list of dicts that was passed in as an argument,
+        with an extra field in each dict named 'success', that indicates if
+        the download was successful, and in case of failures, a field called
+        'failure_reason' that explains why the file could not be downloaded.
+    """
+    raise NotImplementedError
