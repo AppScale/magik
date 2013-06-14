b9d370cbd42ec68d0cba920757521e8fad113fb6
Chris Bunch <shatterednirvana@gmail.com>
2013-06-14 11:48:09 -0700

    added docstring for custom exceptions

--- magik/custom_exceptions.py
+++ magik/custom_exceptions.py
@@ -1,3 +1,7 @@
+""" custom_exceptions.py defines classes that provide Magik-specific Exceptions.
+Right now, we only define one class here, BadConfigurationException, if the
+user improperly tries to interact with magik."""
+
 class BadConfigurationException(Exception):
   """ BadConfigurationException should be thrown whenever a caller invokes a
   method that sets up a Storage service but either (1) passes in invalid
